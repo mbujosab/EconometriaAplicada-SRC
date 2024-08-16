431e0ca74ea25b4f7be2e1dd4b62f46bfd4e092e
Marcos Bujosa <mbujosab@ucm.es>
2024-08-16 16:07:28 +0200

    Algunas modificaciones

--- mycfg.py
+++ mycfg.py
@@ -4,13 +4,7 @@ c.NbConvertApp.notebooks = ["Lecc01.ipynb",
                             "Lecc03.ipynb",
                             "Lecc04.ipynb",
                             "Lecc05.ipynb"]
-
-c.SlidesExporter.mathjax_url = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML"
-
-c.SlidesExporter.jquery_url = "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"
-
-c.SlidesExporter.require_js_url = "https://cdnjs.cloudflare.com/ajax/libs/require.js/2.1.10/require.min.js"
-
-# jupyter nbconvert --config mycfg.py
-# jupyter nbconvert --config mycfg.py  --to slides --reveal-prefix reveal.js
-# jupyter nbconvert --config mycfg.py  --to slides --offline embed --reveal-prefix reveal.js
+c.SlidesExporter.reveal_theme = 'solarized'
+c.SlidesExporter.reveal_scroll = True
+c.SlidesExporter.language_code = 'es'
+c.SlidesExporter.margin = 0
