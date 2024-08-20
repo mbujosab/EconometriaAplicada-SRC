9f6d2f9b1bb7dcf3de3988a43a8a5e8a99ceaf1b
Marcos Bujosa <mbujosab@ucm.es>
2024-08-20 12:47:49 +0200

    carpeta .binder

---
+++ .binder/install.R
@@ -0,0 +1,7 @@
+# Install R packages at build time install.packages() syntax
+install.packages("tfarima")      # librería de José Luis Gallego para Time Series
+install.packages("latticeExtra") # para gráficos con doble eje vertical (doubleYScale)
+install.packages("readr")        # para leer ficheros CSV
+install.packages("ggplot2")      # para el scatterplot (alternaticamente library(tidyverse))
+install.packages("jtools")       # para representación resultados estimación
+install.packages("zoo")          # para generar objetos ts (time series)
