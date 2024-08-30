52d21bf5a3f4b8265602ff43a916331773c9b833
Marcos Bujosa <mbujosab@ucm.es>
2024-08-30 21:18:04 +0200

    Arranque de la lección 6

--- mycfg-GitHubPages.py
+++ mycfg-GitHubPages.py
@@ -3,7 +3,8 @@ c.NbConvertApp.notebooks = ["Lecc01.ipynb",
                             "Lecc02.ipynb",
                             "Lecc03.ipynb",
                             "Lecc04.ipynb",
-                            "Lecc05.ipynb"]
+                            "Lecc05.ipynb",
+                            "Lecc06.ipynb"]
 
 c.SlidesExporter.reveal_theme = 'solarized'
 c.SlidesExporter.reveal_scroll = True
