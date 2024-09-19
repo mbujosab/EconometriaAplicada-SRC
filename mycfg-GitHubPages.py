b99c778ea38d6e3ac7b4f39c7fd02565b14a9ec2
Marcos Bujosa <mbujosab@ucm.es>
2024-09-19 23:22:44 +0200

    Lecciones 7 y 8

--- mycfg-GitHubPages.py
+++ mycfg-GitHubPages.py
@@ -4,11 +4,14 @@ c.NbConvertApp.notebooks = ["Lecc01.ipynb",
                             "Lecc03.ipynb",
                             "Lecc04.ipynb",
                             "Lecc05.ipynb",
-                            "Lecc06.ipynb"]
+                            "Lecc06.ipynb",
+                            "Lecc07.ipynb",
+                            "Lecc08.ipynb"]
 
 c.SlidesExporter.reveal_theme = 'solarized'
 c.SlidesExporter.reveal_scroll = True
 c.SlidesExporter.language_code = 'es'
+c.SlidesExporter.reveal_number = 'c/t'
 
 c.SlidesExporter.mathjax_url = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML"
 
