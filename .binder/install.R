0a453b8c12833b4a76a9f07063df6d7c8a4cef92
Marcos Bujosa <mbujosab@ucm.es>
2025-04-04 10:52:21 +0200

    Nuevo intento de configuración para mybinder

--- .binder/install.R
+++ .binder/install.R
@@ -1,9 +1,2 @@
-# Install R packages at build time install.packages() syntax
-install.packages("tfarima")      # librería de José Luis Gallego para Time Series
-install.packages("latticeExtra") # para gráficos con doble eje vertical (doubleYScale)
-install.packages("readr")        # para leer ficheros CSV
-install.packages("ggplot2")      # para el scatterplot (alternaticamente library(tidyverse))
-install.packages("ggfortify")    # para pintar series temporales
-install.packages("jtools")       # para representación resultados estimación
-install.packages("zoo")          # para generar objetos ts (time series)
-IRkernel::installspec(user = FALSE)
+# install.R
+install.packages("tfarima", repos = "https://cloud.r-project.org")
