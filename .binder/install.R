33af99a53be95ebbfea3a0754fba6d7d42be4edb
Marcos Bujosa <mbujosab@ucm.es>
2025-04-04 14:40:25 +0200

    Cambio definitifo en .binder/environment.yml y .binder/install.R

--- .binder/install.R
+++ .binder/install.R
@@ -7,15 +7,12 @@ install_if_missing <- function(pkg) {
 }
 
 install_if_missing("tfarima")      # librería de José Luis Gallego para Time Series
-install_if_missing("latticeExtra") # para gráficos con doble eje vertical (doubleYScale)
-install_if_missing("readr")        # para leer ficheros CSV
-install_if_missing("ggplot2")      # para el scatterplot (alternaticamente library(tidyverse))
-install_if_missing("jtools")       # para representación resultados estimación
-install_if_missing("zoo")          # para generar objetos ts (time series)
-install_if_missing("pastecs")      # resumen estadísticos descriptivos
-# https://cran.r-project.org/web/packages/pastecs/index.html (stat.desc)
-install_if_missing("knitr")        # presentación de tabla resumen
-# https://cran.r-project.org/web/packages/knitr/index.html (kable)
-# https://bookdown.org/yihui/rmarkdown-cookbook/kable.html
-install_if_missing("moments")      # Perform the Jarque-Bera test
-install_if_missing("tseries")      # Perform the Jarque-Bera test
+#install_if_missing("latticeExtra") # para gráficos con doble eje vertical (doubleYScale)
+#install_if_missing("readr")        # para leer ficheros CSV
+#install_if_missing("ggplot2")      # para el scatterplot (alternaticamente library(tidyverse))
+#install_if_missing("jtools")       # para representación resultados estimación
+#install_if_missing("zoo")          # para generar objetos ts (time series)
+#install_if_missing("pastecs")      # resumen estadísticos descriptivos
+#install_if_missing("knitr")        # presentación de tabla resumen
+#install_if_missing("moments")      # Perform the Jarque-Bera test
+#install_if_missing("tseries")      # Perform the Jarque-Bera test
